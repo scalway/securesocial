905d0b82ce4fc1384d0ab669b72758f3b8e8379f
Bakalov Oleksii <aleo.bakalov@gmail.com>
2016-05-09 19:54:34 +0300

    change play version to 2.5.3

--- project/Common.scala
+++ project/Common.scala
@@ -3,7 +3,7 @@
 object Common {
   def version = "master-SNAPSHOT"
 
-  def playVersion = System.getProperty("play.version", "2.5.1")
+  def playVersion = System.getProperty("play.version", "2.5.3")
 
   def scalaVersion = System.getProperty("scala.version", "2.11.8")
 }
