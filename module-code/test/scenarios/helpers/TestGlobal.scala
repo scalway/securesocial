8d602fe4c214f21eaca4b2fb23821a1ed03fa72f
Bakalov Oleksii <aleo.bakalov@gmail.com>
2016-05-11 19:46:43 +0300

    fix test

--- module-code/test/scenarios/helpers/TestGlobal.scala
+++ module-code/test/scenarios/helpers/TestGlobal.scala
@@ -8,8 +8,11 @@ import securesocial.core.{ BasicProfile, RuntimeEnvironment }
  * Created by dverdone on 8/6/15.
  */
 
-case class TestGlobal() extends RuntimeEnvironment.Default {
+case class TestGlobal()
+/*
+extends RuntimeEnvironment.Default {
   type U = DemoUser
   lazy override val userService: UserService[DemoUser] = null
 
 }
+*/
